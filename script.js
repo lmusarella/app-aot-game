4d2ce18fd77e1e81f0f476dcd8c84046c1c6f8e6
luca.musarella <luca.musarella@lynxspa.com>
2025-09-08 14:57:09 +0200

    Fix

--- script.js
+++ script.js
@@ -537,7 +537,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if (unit.onMission && type !== 'recruit') {
                 const url = './assets/commander_march_sound.mp3';
                 const audioService = new AudioService();
-                gameSoundTrack.backgroundSound.stop();
+               
                 const backgroundSound = await audioService.play(url, { loop: true, volume: 1 });
                 gameSoundTrack.backgroundSound = backgroundSound;
             }
@@ -630,7 +630,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const audioService = new AudioService();
-        gameSoundTrack.backgroundSound.stop();
+        
         const backgroundSound = await audioService.play(url, { loop: true, volume: 1 });
         gameSoundTrack.backgroundSound = backgroundSound;
     };
@@ -1126,6 +1126,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const { innerWidth: vw, innerHeight: vh } = window;
         const rect = elements.hexTooltip.getBoundingClientRect();
         let left = mouseX + offset;
+        
         let top = mouseY + offset;
 
         if (left + rect.width > vw) left = mouseX - rect.width - offset;
