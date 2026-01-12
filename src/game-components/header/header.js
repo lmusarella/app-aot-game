3d6ecf95045e281372d510967d79961bbe0143c2
Luca Musarella <35798100+lmusarella@users.noreply.github.com>
2026-01-12 13:16:08 +0100

    Fix header import for room UI

--- src/game-components/header/header.js
+++ src/game-components/header/header.js
@@ -8,7 +8,7 @@ import showWarningC from '../../game/effects/warningOverlayC.js';
 import { APP_STATE } from '../../core/app-state.js';
 import { supabase } from '../../core/supabase/supabaseClient.js';
 import { showScreen } from '../../ui-components/ui-helpers.js';
-import { stopRoomPresence } from '../room/room-ui.js';
+import { stopRoomPresence } from '../pages/room/room-ui.js';
 import { handleAllyDeath } from '../../game/entity/deaths.js';
 
 const missionCardHead = document.getElementById('mission-head');
